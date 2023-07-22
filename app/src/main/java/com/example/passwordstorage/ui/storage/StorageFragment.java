a4c37f54566d74a89d067d6216a6358d5a3fbc80
KiberGod <stupakov.kirill13@gmail.com>
2023-07-22 16:57:25 +0300

    Added tab menu in home page

--- app/src/main/java/com/example/passwordstorage/ui/storage/StorageFragment.java
+++ app/src/main/java/com/example/passwordstorage/ui/storage/StorageFragment.java
@@ -7,14 +7,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.passwordstorage.R;
+import com.example.passwordstorage.ui.storage.sections.BookmarksFragment;
+import com.example.passwordstorage.ui.storage.sections.CategoriesFragment;
+import com.example.passwordstorage.ui.storage.sections.RecordsFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class StorageFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     private StorageViewModel mViewModel;
 
@@ -25,12 +33,25 @@ public class StorageFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_storage, container, false);
+        View view = inflater.inflate(R.layout.fragment_storage, container, false);
+        addFragment(view);
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    private void addFragment(View view) {
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new RecordsFragment(), "Записи");
+        adapter.addFragment(new CategoriesFragment(), "Категорії");
+        adapter.addFragment(new BookmarksFragment(), "Закладки");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
---
+++ app/src/main/java/com/example/passwordstorage/ui/storage/sections/CategoriesFragment.java
@@ -0,0 +1,21 @@
+package com.example.passwordstorage.ui.storage.sections;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.passwordstorage.R;
+
+public class CategoriesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categories, container, false);
+    }
+}
