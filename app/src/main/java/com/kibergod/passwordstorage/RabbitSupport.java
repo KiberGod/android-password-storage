4e0bb5843550cd0c88164cb872032a938c9c3b16
KiberGod <stupakov.kirill13@gmail.com>
2023-11-25 16:24:37 +0300

    Improved RSS mechanism

--- app/src/main/java/com/kibergod/passwordstorage/RabbitSupport.java
+++ app/src/main/java/com/kibergod/passwordstorage/RabbitSupport.java
@@ -21,24 +21,31 @@ public class RabbitSupport {
 
     // Перелік ідентифікаторів доступних довідкових вікон
     public enum SupportDialogIDs {
-        DIGITAL_OWNER_HIDE_MODE(0),
-        DIGITAL_OWNER_PROTECTED_MODE(1),
-        DIGITAL_OWNER_DATA_DELETION_MODE(2),
-        MAIN_PASSWORD(3),
-        SESSION_PROTECTED(4),
-        INP_CALC_CLEARING(5),
-        DIGITAL_OWNER(6),
-        CREATE_PAGE(7);
+        DIGITAL_OWNER_HIDE_MODE(0, 0, 0, 0),
+        DIGITAL_OWNER_PROTECTED_MODE(0, 1, 1, 0),
+        DIGITAL_OWNER_DATA_DELETION_MODE(0, 2, 2, 0),
+        MAIN_PASSWORD(1, 3, 3, 1),
+        SESSION_PROTECTED(1, 4, 4, 2),
+        INP_CALC_CLEARING(1, 5, 5, 3),
+        DIGITAL_OWNER(0, 6, 6, 4),
+        CREATE_PAGE(2, 7, 7, 5);
 
-        private final int value;
+        private final int sectionId;
+        private final int titleId;
+        private final int mainTextId;
+        private final int iconId;
 
-        SupportDialogIDs(int value) {
-            this.value = value;
+        SupportDialogIDs(int sectionId, int titleId, int mainTextId, int iconId) {
+            this.sectionId = sectionId;
+            this.titleId = titleId;
+            this.mainTextId = mainTextId;
+            this.iconId = iconId;
         }
 
-        public int getValue() {
-            return value;
-        }
+        public int getSectionId() { return sectionId; }
+        public int getTitleId() { return titleId; }
+        public int getMainTextId() { return mainTextId; }
+        public int getIconId() { return iconId; }
     }
 
     /*
@@ -69,15 +76,15 @@ public class RabbitSupport {
         rabbitSupportDialog.setContentView(R.layout.dialog_rabbit_support);
 
         ImageView imageView = rabbitSupportDialog.findViewById(R.id.infoImg);
-        imageView.setImageResource(getImageId(dialogID.getValue()));
+        imageView.setImageResource(getImageId(dialogID.getIconId()));
         imageView.setColorFilter(ContextCompat.getColor(context, R.color.purple), PorterDuff.Mode.SRC_IN);
 
         TextView infoSection = rabbitSupportDialog.findViewById(R.id.infoSection);
         TextView infoTitle = rabbitSupportDialog.findViewById(R.id.infoTitle);
         TextView mainInfoText = rabbitSupportDialog.findViewById(R.id.mainInfoText);
-        infoSection.setText("Розділ: " + getSection(dialogID.getValue()));
-        infoTitle.setText(getTitle(dialogID.getValue()));
-        mainInfoText.setText(getMainText(dialogID.getValue()));
+        infoSection.setText("Розділ: " + getSection(dialogID.getSectionId()));
+        infoTitle.setText(getTitle(dialogID.getTitleId()));
+        mainInfoText.setText(getMainText(dialogID.getMainTextId()));
 
 
         View blurView = rootView.findViewById(blurViewId);
@@ -106,7 +113,7 @@ public class RabbitSupport {
             ((ViewGroup) cancelButton.getParent()).removeView(cancelButton);
         }
 
-        if (getMainText(dialogID.getValue()).length() > 400) {
+        if (getMainText(dialogID.getMainTextId()).length() > 400) {
             ScrollView scrollView = rabbitSupportDialog.findViewById(R.id.scrollArea);
 
             ViewGroup.LayoutParams layoutParams = scrollView.getLayoutParams();
@@ -121,12 +128,7 @@ public class RabbitSupport {
     private static String getSection(int id) {
         return new String[]{
                 "Налаштування > Цифровий власник",
-                "Налаштування > Цифровий власник",
-                "Налаштування > Цифровий власник",
                 "Налаштування",
-                "Налаштування",
-                "Налаштування",
-                "Налаштування > Цифровий власник",
                 "Додати"
         }[id];
     }
@@ -154,7 +156,7 @@ public class RabbitSupport {
                 "Це ваш особистий ключ для входу у сховище. Він вводиться у калькуляторі, тому має містити лише:\n\n- цифри (0-9)\n- символи ('.', '-', '+')\n\nТакож пароль не може бути порожнім або перевищувати довжину в 9 знаків.",
                 "Будучи ввімкненою, дана опція повертатиме вас до калькулятора щоразу, коли:\n\n- програму було згорнуто\n- екран пристрою погас\n- пристрій вимкнуто\n\nПідвищує загальний рівень безпеки ваших даних.",
                 "Дане налаштування дозволяє спростити вхід до сховища, шляхом видалення попередніх даних вводу у калькуляторі (якщо увімкнено).\n\nПідвищує безпеку використання програми при сторонніх людях.",
-                "Цифровий власник - це підсистема Rabbit Hole, що містить алгоритми автоматизованого керування програмою. На даний момент має 3 режими роботи:\n\n- приховання даних\n- захист входу\n - видалення даних\n\nЦільовою задачею даної підсистеми є підвищення безпеки шляхом використання нестандартних методів.\n\nБудьте обережні при використанні та взаємодії з Цифровим власником. Уважно ознайомтесь з деталями обраного режиму, перш ніж його вмикати. Ця підсистема є вкрай небезпечною, допустивши помилку, ви ризикуєте втратити свої дані або навіть доступ до сховища.\n\nВикористовуйте тільки у тому разі, якщо точно знаєте, що робите.",
+                "Цифровий власник - це підсистема Rabbit Hole, що містить алгоритми автоматизованого керування програмою. На даний момент має 3 режими роботи:\n\n- приховання даних\n- захист входу\n- видалення даних\n\nЦільовою задачею даної підсистеми є підвищення безпеки шляхом використання нестандартних методів.\n\nБудьте обережні при використанні та взаємодії з Цифровим власником. Уважно ознайомтесь з деталями обраного режиму, перш ніж його вмикати. Ця підсистема є вкрай небезпечною, допустивши помилку, ви ризикуєте втратити свої дані або навіть доступ до сховища.\n\nВикористовуйте тільки у тому разі, якщо точно знаєте, що робите.",
                 "Даний розділ надає вам можливість створювати записи та їх категорії. \n\nХованка - умовна узагальнююча назва. Запис є основним об'єктом зберігання ваших даних. Категорії призначені для більш гнучкого керування записами.\n\nЩоб почати створювати нову хованку, просто поверніться до відповідного меню та натисніть будь-яку кнопку."
         }[id];
     }
@@ -162,8 +164,6 @@ public class RabbitSupport {
     // Набір ідентифікаторів картинок
     private static int getImageId(int id) {
         return new int[]{
-                R.drawable.vector__modern_rabbit,
-                R.drawable.vector__modern_rabbit,
                 R.drawable.vector__modern_rabbit,
                 R.drawable.vector__vertical_key,
                 R.drawable.vector__phone_lock,
