af69eea51c13572fa24b4c2fa96578c916137a21
KiberGod <stupakov.kirill13@gmail.com>
2024-01-22 02:46:35 +0200

    Updated app info page

--- app/src/main/java/com/kibergod/passwordstorage/ui/pages/tools/AppInfoFragment.java
+++ app/src/main/java/com/kibergod/passwordstorage/ui/pages/tools/AppInfoFragment.java
@@ -1,5 +1,6 @@
 package com.kibergod.passwordstorage.ui.pages.tools;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.kibergod.passwordstorage.BuildConfig;
 import com.kibergod.passwordstorage.R;
 public class AppInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_app_info, container, false);
 
+        setVersion(view);
         return view;
+    }
+
+    private void setVersion(View view) {
+        TextView versionTextView = view.findViewById(R.id.programVersionView2);
+        versionTextView.setText("v."+ BuildConfig.VERSION_NAME);
+        versionTextView.setPaintFlags(versionTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
